32ee7568ff50331db5c596b2df4a05375d3f368e
Lukasz184396 <zawadalukasz88@gmail.com>
2018-12-27 10:17:59 +0100

    Added springboot app project wth cloud dependency

---
+++ src/main/java/com/zawadal/plurarsightspringcloudm2configclient/PlurarsightSpringcloudM2ConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.zawadal.plurarsightspringcloudm2configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlurarsightSpringcloudM2ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlurarsightSpringcloudM2ConfigClientApplication.class, args);
+	}
+
+}
+
